eed8009fed500ae5877662f5a85c98e5962d11a2
kenxx16 <ken.x.x@yandex.ru>
2024-10-17 19:33:37 +0300

    мы смогли начать

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { DashboardTileComponent } from './common/dashboard-tile/dashboard-tile.component';
+import { ProfileService } from './data/services/profile.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardTileComponent],
+  
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'notes_s';
+  profileService: ProfileService = inject(ProfileService);
+  profiles: any = []
+
+  constructor() {
+    this.profileService.getTestAccounts()
+    .subscribe(val => {
+      this.profiles = val;
+    })
+  }
 }
